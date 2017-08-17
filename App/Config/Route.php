3e639edb9b83f40b583a109b054e500024592811
scila1996 <trungnt.hust.96@gmail.com>
2017-08-17 12:58:02 +0700

    Signed-off-by: scila1996 <trungnt.hust.96@gmail.com>

--- App/Config/Route.php
+++ App/Config/Route.php
@@ -3,5 +3,5 @@
 use System\Core\Config;
 use System\Libraries\Router\RouteCollector;
 
-Config::$route->get('/home', ["Home"]);
+Config::$route->get('/home', ["Home", "index"]);
 Config::$route->get('/about', ["Home", "about"]);
